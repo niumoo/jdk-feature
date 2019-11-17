1073a8794dae8b8adec9371adf8e8465fd98af7e
niumoo <niumoo@126.com>
2019-11-17 21:21:22 +0800

    Jdk8Stream.java

--- src/main/java/net/codingme/feature/jdk8/Jdk8Stream.java
+++ src/main/java/net/codingme/feature/jdk8/Jdk8Stream.java
@@ -5,10 +5,7 @@ import org.junit.Test;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.IntSummaryStatistics;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -60,6 +57,94 @@ public class Jdk8Stream {
     }
 
     @Test
+    public void streamDemo(){
+        List<String> nameList = Arrays.asList("Darcy", "Chris", "Linda", "Sid", "Kim", "Jack", "Poul", "Peter");
+        // 1. 筛选出名字长度为4的
+        // 2. 名字前面拼接 This is
+        // 3. 遍历输出
+        nameList.stream()
+                .filter(name -> name.length() == 4)
+                .map(name -> "This is "+name)
+                .forEach(name -> System.out.println(name));
+    }
+
+    /**
+     * 转换成为大写然后收集结果，遍历输出
+     */
+    @Test
+    public void toUpperCaseDemo() {
+        List<String> nameList = Arrays.asList("Darcy", "Chris", "Linda", "Sid", "Kim", "Jack", "Poul", "Peter");
+        List<String> upperCaseNameList = nameList.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+        upperCaseNameList.forEach(name -> System.out.print(name + ","));
+    }
+
+    /**
+     * 把数字值乘以2
+     */
+    @Test
+    public void mapTest() {
+        List<Integer> numberList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        List<Integer> collect = numberList.stream()
+                .map(number -> number * 2)
+                .collect(Collectors.toList());
+        collect.forEach(number -> System.out.print(number + ","));
+    }
+
+
+    /**
+     * flatmap把对象扁平化
+     */
+    @Test
+    public void flatMapTest() {
+        Stream<List<Integer>> inputStream = Stream.of(
+                Arrays.asList(1),
+                Arrays.asList(2, 3),
+                Arrays.asList(4, 5, 6)
+        );
+        List<Integer> collect = inputStream
+                .flatMap((childList) -> childList.stream())
+                .collect(Collectors.toList());
+        collect.forEach(number -> System.out.print(number + ","));
+    }
+
+    /**
+     * 遍历输出
+     */
+    @Test
+    public void forEachTest(){
+        List<Integer> numberList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        numberList.stream().forEach(number -> System.out.print(number+","));
+    }
+
+    /**
+     * filter 数据筛选
+     * 筛选出偶数数字
+     */
+    @Test
+    public void filterTest() {
+        List<Integer> numberList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        List<Integer> collect = numberList.stream()
+                .filter(number -> number % 2 == 0)
+                .collect(Collectors.toList());
+        collect.forEach(number -> System.out.print(number + ","));
+    }
+
+    /**
+     * 查找第一个数据
+     * 返回的是一个 Optional 对象
+     */
+    @Test
+    public void findFirstTest(){
+        List<Integer> numberList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        Optional<Integer> firstNumber = numberList.stream()
+                .findFirst();
+        System.out.println(firstNumber.orElse(-1));
+    }
+
+
+    @Test
     public void test1() {
 
         // 不适用流操作
@@ -93,7 +178,7 @@ public class Jdk8Stream {
      * map，注意，这里的map是进行1:1的转换映射，一般用于从前到后的内容和类型修改
      */
     @Test
-    public void mapTest() {
+    public void mapTest2() {
         List<String> skills = Arrays.asList("java", "golang", "c++", "c", "python");
         // 添加中括号后输出
         skills.stream().map((skill) -> "[" + skill + "]").forEach(System.out::print);
@@ -122,7 +207,7 @@ public class Jdk8Stream {
      * filter 过滤例子
      */
     @Test
-    public void filterTest() {
+    public void filterTest2() {
         List<String> skills = Arrays.asList("java", "golang", "c++", "c", "python");
         List<String> strings = skills.stream().filter(skill -> skill.length() > 4).collect(Collectors.toList());
         strings.forEach(skill -> System.out.println(skill));

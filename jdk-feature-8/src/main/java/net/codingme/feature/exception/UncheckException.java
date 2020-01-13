def647f2956434a900be4d195e9cbe08a75248a9
niumoo <niumoo@126.com>
2020-01-13 21:52:27 +0800

    delete file

--- jdk-feature-8/src/main/java/net/codingme/feature/exception/UncheckException.java
+++
@@ -1,9 +0,0 @@
-package net.codingme.feature.exception;
-
-public class UncheckException {
-
-    public static void main(String[] args) {
-        // 非受检异常
-        throw new RuntimeException("asdasd");
-    }
-}

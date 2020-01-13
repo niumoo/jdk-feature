3ca8825f26309986f948d78683517face338b2c0
niumoo <niumoo@126.com>
2020-01-13 21:44:48 +0800

    reafct jdk-feature-8

---
+++ jdk-feature-8/src/main/java/net/codingme/feature/exception/UncheckException.java
@@ -0,0 +1,9 @@
+package net.codingme.feature.exception;
+
+public class UncheckException {
+
+    public static void main(String[] args) {
+        // 非受检异常
+        throw new RuntimeException("asdasd");
+    }
+}

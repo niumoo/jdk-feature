9ed2689276e3142adab1ffdd89328996081ff1bd
niumoo <niumoo@126.com>
2021-02-23 17:46:29 +0800

    Java 12 feature

---
+++ jdk-feature-12/src/com/wdbyte/feature/jdk12/InstanceofApi.java
@@ -0,0 +1,16 @@
+//package com.wdbyte.feature.jdk12;
+//
+///**
+// * JDK 14 才有的特性，这里注释
+// * @author darcy
+// * @date 2021/02/17
+// */
+//public class InstanceofApi {
+//
+//    public static void main(String[] args) {
+//        Object obj = new String("Hello world");
+//        if (obj instanceof String s) {
+//            System.out.println(s.length());
+//        }
+//    }
+//}
---
+++ jdk-feature-12/src/com/wdbyte/feature/jdk12/StreamAPI.java
@@ -0,0 +1,21 @@
+package com.wdbyte.feature.jdk12;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author darcy
+ * @date 2021/02/17
+ */
+public class StreamAPI {
+
+    public static void main(String[] args) {
+        //double mean = Stream.of(1, 2, 3, 4, 5)
+        //    .collect(Collectors.teeing(
+        //        summingDouble(i -> i),
+        //        counting(),
+        //        (sum, n) -> sum / n));
+        //
+        //System.out.println(mean);
+    }
+}
